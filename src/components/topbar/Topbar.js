4a276dcbc3b844358d83a43d9881246f47532bb9
Puneeth <puneet.shivram@gmail.com>
2021-06-22 15:03:25 +0530

    Added navbar menu

--- src/components/topbar/Topbar.js
+++ src/components/topbar/Topbar.js
@@ -1,5 +1,33 @@
 import React from "react";
+import { NotificationsNone, Settings, Language } from "@material-ui/icons";
+import "./topbar.css";
 
 export default function Topbar() {
-  return <div>Topbar</div>;
+  return (
+    <div className="topbar">
+      <div className="topbarWrapper">
+        <div className="topLeft">
+          <span className="logo">Admin</span>
+        </div>
+        <div className="topRight">
+          <div className="topbarIconContainer">
+            <NotificationsNone />
+            <span className="topIconBadge">2</span>
+          </div>
+          <div className="topbarIconContainer">
+            <Language />
+            <span className="topIconBadge">2</span>
+          </div>
+          <div className="topbarIconContainer">
+            <Settings />
+          </div>
+          <img
+            src="https://cdn2.iconfinder.com/data/icons/avatars-99/62/avatar-370-456322-512.png"
+            alt="profile-picture"
+            className="topAvatar"
+          />
+        </div>
+      </div>
+    </div>
+  );
 }
