6489b3103a2ffe89d1ff043a1ca776f62d63986b
Puneeth <puneet.shivram@gmail.com>
2021-06-23 20:13:21 +0530

    Added info widgets cards

---
+++ src/pages/Home.js
@@ -0,0 +1,9 @@
+import FetauredInfo from "../components/featuredInfo/FetauredInfo";
+import "./home.css";
+export default function Home() {
+  return (
+    <div className="home">
+      <FetauredInfo />
+    </div>
+  );
+}
