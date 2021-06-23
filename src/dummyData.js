8810f7aae4c0a80e55add966a6879e51097a7dd1
Puneeth <puneet.shivram@gmail.com>
2021-06-23 22:24:34 +0530

    Pushed chart files

---
+++ src/dummyData.js
@@ -0,0 +1,74 @@
+export const userDataAnalytics = [
+  {
+    name: "Jan",
+    ActiveUsers: 4000,
+    InActiveUsers: 2400,
+    amt: 2400,
+  },
+  {
+    name: "Feb",
+    ActiveUsers: 3000,
+    InActiveUsers: 1398,
+    amt: 2210,
+  },
+  {
+    name: "Mar",
+    ActiveUsers: 2000,
+    InActiveUsers: 9800,
+    amt: 2290,
+  },
+  {
+    name: "Apr",
+    ActiveUsers: 2780,
+    InActiveUsers: 3908,
+    amt: 2000,
+  },
+  {
+    name: "May",
+    ActiveUsers: 1890,
+    InActiveUsers: 4800,
+    amt: 2181,
+  },
+  {
+    name: "Jun",
+    ActiveUsers: 5390,
+    InActiveUsers: 3800,
+    amt: 2500,
+  },
+  {
+    name: "Jul",
+    ActiveUsers: 3490,
+    InActiveUsers: 4300,
+    amt: 2100,
+  },
+  {
+    name: "Aug",
+    ActiveUsers: 3590,
+    InActiveUsers: 4300,
+    amt: 2100,
+  },
+  {
+    name: "Sep",
+    ActiveUsers: 4490,
+    InActiveUsers: 7300,
+    amt: 2100,
+  },
+  {
+    name: "Oct",
+    ActiveUsers: 3490,
+    InActiveUsers: 4300,
+    amt: 2100,
+  },
+  {
+    name: "Nov",
+    ActiveUsers: 2490,
+    InActiveUsers: 4300,
+    amt: 2100,
+  },
+  {
+    name: "Dec",
+    ActiveUsers: 4890,
+    InActiveUsers: 4300,
+    amt: 2100,
+  },
+];
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,12 @@
 import FetauredInfo from "../components/featuredInfo/FetauredInfo";
+import Chart from "../components/chart/Chart";
+import { userDataAnalytics } from "../dummyData";
 import "./home.css";
 export default function Home() {
   return (
     <div className="home">
       <FetauredInfo />
+      <Chart data={userDataAnalytics} title="User Analytics" />
     </div>
   );
 }
