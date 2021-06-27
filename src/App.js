a76cacbc04438ddf22f19c165722961b01b6ae19
Puneeth <puneet.shivram@gmail.com>
2021-06-27 22:59:23 +0530

    Added Data grid

--- src/App.js
+++ src/App.js
@@ -1,15 +1,26 @@
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Topbar from "./components/topbar/Topbar";
 import Sidebar from "./components/sidebar/Sidebar";
+import UserList from "./components/userList/UserList";
 import Home from "./pages/Home";
 import "./app.css";
 function App() {
   return (
     <div className="App">
-      <Topbar />
-      <div className="container">
-        <Sidebar />
-        <Home />
-      </div>
+      <Router>
+        <Topbar />
+        <div className="container">
+          <Sidebar />
+          <Switch>
+            <Route exact path="/">
+              <Home />
+            </Route>
+            <Route exact path="/users">
+              <UserList />
+            </Route>
+          </Switch>
+        </div>
+      </Router>
     </div>
   );
 }
